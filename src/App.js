b785af8d42ebd3ed253c522a6f564e29cd3f41d4
Roman Nikonov <11034375+nic11@users.noreply.github.com>
2021-10-16 19:45:08 +0300

    descriptions

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Counter description="anime" start={0} />
-        <Counter description="sloc" start={10} />
+        <Counter description="Fist counter" start={0} />
+        <Counter description="Second counter" start={10} />
       </div>
     );
   }
