b17b054a609f680c6985f7bad4b63f617f395ee6
Roman Nikonov <11034375+nic11@users.noreply.github.com>
2021-10-16 15:17:58 +0300

    changes 1

--- src/App.js
+++ src/App.js
@@ -3,18 +3,11 @@ import './App.css';
 import Counter from './Counter';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.counters = [
-      <Counter description="anime" start="0" />,
-      <Counter description="sloc" start="10" />
-    ];
-  }
-
   render() {
     return (
       <div className="App">
-        {this.counters}
+        <Counter description="anime" start="0" />
+        <Counter description="sloc" start="10" />
       </div>
     );
   }
